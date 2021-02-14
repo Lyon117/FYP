e2d9ae7d42374b001b13a0a96095c08e3f0d2a66
murasakiakari <71558600+murasakiakari@users.noreply.github.com>
2021-02-14 15:58:27 +0800

    Add files via upload

--- Code & Configuration/Saveuser.py
+++ Code & Configuration/Saveuser.py
@@ -9,18 +9,7 @@ cursor = db.cursor()
 def main(uid, student_name, student_id):
     uid = [str(i) for i in uid]
     uid = ''.join(uid)
-    cursor.execute(f'SELECT id FROM reg_user WHERE rfid_uid={uid}')
-    cursor.fetchone()
-    if cursor.rowcount >= 1:
-        sleep(1)
-        sql_insert = 'UPDATE reg_user SET name =%s WHERE rfid_uid=%s'
-        cursor.execute(sql_insert, (student_name, uid))
-        sql_insert = 'UPDATE reg_user SET sid =%s WHERE rfid_uid=%s'
-        cursor.execute(sql_insert, (str(student_id), uid))
-        sql_insert = 'UPDATE reg_user SET pw =%s WHERE rfid_uid=%s'
-        cursor.execute(sql_insert, ('12345678', uid))
-    else:
-        sql_insert = 'INSERT INTO reg_user (name, rfid_uid, sid, pw) VALUES (%s,%s,%s,%s)'
-        cursor.execute(sql_insert, (student_name, uid, student_id, '12345678'))
+    sql_insert = 'INSERT INTO reg_user (STUDENT_ID, STUDENT_NAME, UID) VALUES (%s,%s,%s)'
+    cursor.execute(sql_insert, (student_id, student_name, uid))
     db.commit()
     sleep(2)
